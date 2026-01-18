8ecf852d7a4b7bfbd2cf56af8c7f1d4752871fdc
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2026-01-18 06:10:50 -0500

    update header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, input, signal } from '@angular/core';
+import { NgClass } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { NgIcon } from "@ng-icons/core"
+import { heroBars3Solid } from "@ng-icons/heroicons/solid"
 
 @Component({
   selector: 'header[app-header]',
-  imports: [],
+  imports: [RouterLink, NgIcon],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  variant = input<'white' | 'blue'>('white');
+  icon = signal(heroBars3Solid);
+  sidebarOpen = signal(false);
 
+  toggleSidebar() {
+    this.sidebarOpen.update((open) => !open);
+  }
+
+  @HostBinding('class.white') get whiteClass() {
+    return this.variant() === 'white';
+  }
+
+  @HostBinding('class.blue') get blueClass() {
+    return this.variant() === 'blue';
+  }
 }
