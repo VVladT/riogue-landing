e7f0a0432dfb92b66d9dddbc38b45ac80ee2983b
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2026-01-19 16:52:44 -0500

    minor changes

--- src/app/page/home/brands/brands.component.ts
+++ src/app/page/home/brands/brands.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Brands } from './brands.model';
 import { MarqueeComponent } from "@shared/components/marquee/marquee.component";
+import { SectionComponent } from "@shared/components/section/section.component";
 
 @Component({
   selector: 'app-brands',
-  imports: [MarqueeComponent],
+  imports: [MarqueeComponent, SectionComponent],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css',
 })
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,15 +1,12 @@
 import { Component, effect, inject, signal } from '@angular/core';
-import { SectionComponent } from "@shared/components/section/section.component";
 import { HeroComponent } from "./hero/hero.component";
-import { SliderComponent } from "@shared/components/slider/slider.component";
-import { CardOverlayComponent } from "@shared/components/card-overlay/card-overlay.component";
 import { BrandsComponent } from "./brands/brands.component";
 import { HomeStore } from './home.store';
 import { SolutionsComponent } from "./solutions/solutions.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SectionComponent, HeroComponent, SliderComponent, CardOverlayComponent, BrandsComponent, SolutionsComponent],
+  imports: [HeroComponent, BrandsComponent, SolutionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   providers: [HomeStore],
