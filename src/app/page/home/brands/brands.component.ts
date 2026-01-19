09e9adc2b22022e12a1a8886f7060ba4dc963b55
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2026-01-19 09:04:08 -0500

    fix invalid import

--- src/app/page/home/brands/brands.component.ts
+++ src/app/page/home/brands/brands.component.ts
@@ -1,11 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Brands } from './brands.model';
 import { MarqueeComponent } from "@shared/components/marquee/marquee.component";
-import { NgClass } from "../../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-brands',
-  imports: [MarqueeComponent, NgClass],
+  imports: [MarqueeComponent],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css',
 })
