7235efcdf54eb8f6ee22481b4010ef5e4761fa74
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2026-01-18 19:13:24 -0500

    update hero to make it responsive

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SectionComponent } from "@shared/components/section/section.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SectionComponent],
+  imports: [SectionComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
