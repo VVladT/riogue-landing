816132471212732a13d9c04505f8807208f5ea3a
Jaime López <exorsnine@gmail.com>
2026-01-20 14:56:52 -0500

    Replaced some img identity placeholder

--- src/app/page/about-us/company-identity/company-identity.service.ts
+++ src/app/page/about-us/company-identity/company-identity.service.ts
@@ -11,19 +11,19 @@ export class CompanyIdentityService {
     {
       name: 'Misión',
       description: 'Nuestro propósito fundamental, es el de ofrecer un servicio de telecomunicaciones e informática profesional, para todo índole de nivel empresarial, aportando la confianza y tranquilidad necesaria a la hora de adquirir soluciones de calidad a un precio razonable, en un mundo complejo que evoluciona a velocidad en las nuevas tecnologías de comunicación',
-      imgUrl: `${this.basePath}/estructuras-metalicas.jpg`,
+      imgUrl: `${this.basePath}/mision.webp`,
       imgOnRight: false
     },
     {
       name: 'Visión',
       description: 'Buscamos ser una empresa de referencia, que camina con el cambio de la tecnología y la sociedad, dando a conocer las posibilidades de los estándares y tecnologías libres. Esta labor se debe desempeñar de forma ética y satisfactoria para nosotros, nuestros clientes y el resto de la sociedad',
-      imgUrl: `${this.basePath}/instalacion-cerco-electrico.jpg`,
+      imgUrl: `${this.basePath}/reparacion-de-computadoras.jpeg`,
       imgOnRight: true
     },
     {
       name: 'Objetivo',
       description: 'Establecer relaciones de negocios sólidas y permanentes con nuestros clientes, manteniéndonos como sus socios estratégicos, brindándoles soluciones outsourcing para sus negocios locales y offshore',
-      imgUrl: `${this.basePath}/reparacion-de-computadoras.jpeg`,
+      imgUrl: `${this.basePath}/objetivos.webp`,
       imgOnRight: false
     },
   ]
